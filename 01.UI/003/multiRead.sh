f4a627751315a56fb06c66eac0bc5e5d7f71ecc4
Yoonsoo Lee <bubblemint@sk.com>
2016-04-14 10:15:17 +0900

    003. 키보드에서 사용자 키 입력을 획득해서 변숫값으로 이용하

---
+++ 01.UI/003/multiRead.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+printf "Enter your ID: " 
+read id 
+
+printf "Enter your NAME: " 
+read name
+
+printf "Enter your NUMBER: " 
+read number
+
+
+
+
+echo "Now your ID is $id"
+echo "NAME: $name, NUMBER: $number"
---
+++ 01.UI/003/read.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+/bin/echo -n "Enter your ID: "
+read id
+
+echo "Now your ID is $id"
