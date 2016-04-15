124bd55eb4f6c5b60beab5a8934ffc439697eca3
Yoonsoo Lee <bubblemint@sk.com>
2016-04-15 16:58:52 +0900

    008. 표시 문자 색 변경하기

---
+++ 01.UI/008/color.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Script Start."
+
+#배경을 회색(47), 문자색을 빨강(31)으로 바꿈
+#Mac Ver.
+echo "\033[47;31m Importnt Message \033[0m"
+
+#Linux Ver.
+#echo -e "\033[47;31m Importnt Message \033[0m"
+
+echo "Script End"
