827bf8e48cbe1f502915d0b0cab58f8194022161
Yoonsoo Lee <bubblemint@sk.com>
2016-04-15 17:08:48 +0900

    Message 오타 교정

--- 01.UI/008/color.sh
+++ 01.UI/008/color.sh
@@ -4,7 +4,7 @@ echo "Script Start."
 
 #배경을 회색(47), 문자색을 빨강(31)으로 바꿈
 #Mac Ver.
-echo "\033[47;31m Importnt Message \033[0m"
+echo "\033[47;31m Important Message \033[0m"
 
 #Linux Ver.
 #echo -e "\033[47;31m Importnt Message \033[0m"
